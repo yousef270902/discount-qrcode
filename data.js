7a275c90d74ad9f74237e5bb357c1b805e4673f8
youssef Wahba <150840439+yousef270902@users.noreply.github.com>
2026-01-13 17:59:52 +0200

    Create data.js

---
+++ data.js
@@ -0,0 +1,21 @@
+window.APP_DATA = {
+  settings: {
+    discount: 25,               // نسبة الخصم
+    expire: "2026-01-31"        // تاريخ الانتهاء
+  },
+
+  products: {
+    1: { name: "منتج 1", price: 100 },
+    2: { name: "منتج 2", price: 200 },
+    3: { name: "منتج 3", price: 300 },
+    4: {name : "منتج 4" , price:400},
+    5: {name : "منتج 5" , price:500},
+    6: {name : "منتج 6" , price:600},
+    7: {name : "منتج 7" , price:700},
+    8: {name : "منتج 8" , price:800},
+    9: {name : "منتج 9" , price:900},
+    10: {name : "منتج 10" , price:1000}
+    
+    
+  }
+};
